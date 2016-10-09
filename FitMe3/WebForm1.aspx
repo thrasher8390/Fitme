0a0aebea3808915c955302bb014fa9e4b75cdf3c
Derek Thrasher <Thrasher8390@gmail.com>
2016-10-08 21:54:45 -0400

    Initial Commit

---
+++ FitMe3/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="FitMe3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:SqlDataSource ID="FitMeUsers" runat="server" ConnectionString="<%$ ConnectionStrings:FitMeDatabase %>" ProviderName="<%$ ConnectionStrings:FitMeDatabase.ProviderName %>" SelectCommand="SELECT fitmeusers.* FROM fitmeusers"></asp:SqlDataSource>
+    
+    </div>
+        <p>
+            Here is a website with 2 users</p>
+        <asp:TextBox ID="tbUserName" runat="server">User Name</asp:TextBox>
+        <asp:TextBox ID="tbFirstName" runat="server">First Name</asp:TextBox>
+        <asp:Button ID="btnSaveUser" runat="server" OnClick="btnSaveUser_Click" Text="SaveUser" />
+        <br />
+        <asp:GridView ID="gvFitMeUsers" runat="server" AllowPaging="True" AllowSorting="True" DataSourceID="FitMeUsers">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ FitMe3/WebForm1.aspx.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FitMe3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSaveUser_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (MySqlConnection cn = new MySqlConnection(FitMeUsers.ConnectionString))
+                {
+                    MySqlCommand cmd = new MySqlCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandText = "INSERT INTO fitmeusers (userName,firstName) VALUES(\'" + tbUserName.Text + "\',\'" + tbFirstName.Text + "\')";
+                    cn.Open();
+                    int numRowsUpdated = cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            gvFitMeUsers.DataBind();
+        }
+    }
+}
